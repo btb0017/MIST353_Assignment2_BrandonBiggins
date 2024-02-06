96304db704e6b89d0ff06f19e1af9dbae9786ea6
btb0017 <btb0017@mix.wvu.edu>
2024-02-06 14:48:27 -0500

    Created Pages for Assignment 2

---
+++ Assignment2_EcoInvest_BrandonBiggins/Assignment2_EcoInvest/Pages/Information.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Assignment2_EcoInvest.Pages
+{
+    public class InformationModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
